550e9789cb9ed80853c05111eb98d0871ba4f9c6
Thakur-Abhi-Rana <abhishekpundirmzn34@gmail.com>
2025-11-08 10:04:27 +0000

    conversion data types

---
+++ 01_basic/03conversion.js
@@ -0,0 +1,28 @@
+let score="44axd"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber= Number(score)
+
+console.log(typeof (valueInNumber));
+console.log((valueInNumber));
+
+//"33"=>33
+//"33grg"=> nan
+//true=> 1; false=>0
+
+let isLoggedIn="abhishek"
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1=> true; 0=> false
+//""=>false
+//"abhishek"=> true
+
+
+let someNumber=44
+
+let stringNumber= String(someNumber)
+console.log(stringNumber);
