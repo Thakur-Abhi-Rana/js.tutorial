26f3587034a73cf11429f3e36f5eabe92ba5aec4
Thakur-Abhi-Rana <abhishekpundirmzn34@gmail.com>
2025-11-08 05:07:12 +0000

    variable update

---
+++ 01_basic/01 variable.js
@@ -0,0 +1,8 @@
+const accountId=143414
+let accountEmail = "abhishek@google.com"
+var accontpassword = "381368"
+accontCity = "mzn"
+
+accontId = 2
+
+console.log(accountId);
---
+++ 01_basic/01variable.js
@@ -0,0 +1,22 @@
+const accountId=143414
+let accountEmail = "abhishek@google.com"
+var accontpassword = "381368"
+accontCity = "mzn"
+let accontState;
+
+// accountId = 2
+
+accountEmail="bajdh.com"
+accountPassword="17367246"
+accontCity="sre"
+
+console.log(accountId);
+
+
+
+/*
+prefer not to use var beacuase of issue in block and functional
+*/ 
+
+console.table([accountId,accountEmail,accountPassword,accontCity,accontState])
+
