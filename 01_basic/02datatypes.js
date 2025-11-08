673bd2edb8a288206e926206a59703195db3ae81
Thakur-Abhi-Rana <abhishekpundirmzn34@gmail.com>
2025-11-08 09:16:58 +0000

    datatypes

---
+++ 01_basic/02datatypes.js
@@ -0,0 +1,33 @@
+"use strict" // treat all js code as newer version
+
+//alert(3+3)//we are using nodejs,not browser
+
+console.log(3+3);// code readability
+console.log("Abhishek")
+
+
+
+let name="Abhishek"
+let age=23
+let isLoggedIN=false
+
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean=>true/flase
+//null=> standalone value
+//undefined=>value not assing
+//symbol => unique
+
+
+//object
+
+
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+//null=>object
+//undefined=> character
+
+
