c35f76f765a4cea912918f95bf08126144d70905
Thakur-Abhi-Rana <abhishekpundirmzn34@gmail.com>
2025-11-09 11:36:11 +0000

    datatypes summary

---
+++ 01_basic/04datatypes.js
@@ -0,0 +1,28 @@
+// primitive
+
+//7types: String,number, boolean, null,undefined, symbol,BigInt
+
+
+const id= Symbol('123')
+const anotherId= Symbol('123')
+ //console.log(id==anotherId);
+
+// const bigNumber=342532643757n
+ 
+
+
+//reference type(non primitve)
+
+// array,object,funtions
+
+const heros= ["shktiman","naagraj","arjun"]
+
+let myObj={
+    name:"abhishek",
+    age:23,
+}
+const myFunction = function(){
+    console.log("hello world");
+    
+}
+console.log(typeof String);
