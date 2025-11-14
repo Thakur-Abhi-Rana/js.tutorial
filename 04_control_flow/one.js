70e771bee15b171af2a5b65ba0f185be29cf3578
Thakur-Abhi-Rana <abhishekpundirmzn34@gmail.com>
2025-11-14 09:13:41 +0000

    control flow basic

---
+++ 04_control_flow/one.js
@@ -0,0 +1,71 @@
+// if
+
+
+//if (true){
+    
+//}
+
+// const isUserloggedIN = true
+// const temparature = 60
+
+// if ( temparature<50){
+//     console.log("less than 50");
+// }
+// else {
+// console.log("temperature is greater than 50");
+// }
+
+
+//2<=2
+//2=== "2"
+//<, >, <=, ==,!=,===,!==
+
+
+// const score = 101
+
+// if(score > 100){
+//     let power = "fly"
+//     console.log(` user power: ${power}`);
+    
+    
+// }
+//  console.log(` user power: ${power}`);
+
+
+
+// const balance = 1000
+
+// // if (balance >500) console.log("test"),console.log("test2");
+
+// if (balance<500){
+//     console.log("less than ");
+    
+// }else if (balance > 750){
+//     console.log("less than 750");
+    
+// } else if (balance < 900){
+//     console.log("less than 750");
+    
+// }else{
+//     console.log("less than 1200");
+    
+// }
+
+
+const UserloggedIN = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+// if (UserloggedIN && debitCard && 2==3){
+//     console.log("allow to buy course");
+    
+// }
+
+if (loggedInFromGoogle || loggedInFromEmail ){
+    console.log("user logged in");
+    
+}else {
+    console.log("no UserloggedIN");
+    
+}
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+//         break;
+//         default:
+//             break;
+// }
+
+
+const month = "march"
+
+switch (month) {
+  case "jan":
+        console.log("january");
+         break;  c
+  case "feb":
+        console.log("feb");
+         break;  
+  case "march":
+        console.log("march");
+         break;  
+  case "apr":
+        console.log("apr");
+         break;  
+
+    default:
+      console.log("default value");
+      
+        break;
+}
